9780ddc87593b295b6989419ddbb0abcbac99e51
kim1037 <107454420+kim1037@users.noreply.github.com>
2025-05-18 00:09:28 +0800

    refactor: add generics to sendResponse

--- src/utils/sendResponse.ts
+++ src/utils/sendResponse.ts
@@ -1,14 +1,19 @@
 import type { Response } from "express";
 
 // 回應輔助函數
-export const sendResponse = (
+export const sendResponse = <D = any, P = any>(
   res: Response,
   statusCode: number,
   message: string,
   success: boolean,
-  data?: any,
-  pagination?: any,
-) => {
+  data?: D,
+  pagination?: P,
+): Response<{
+  message: string;
+  status: boolean;
+  data?: D;
+  pagination?: P;
+}> => {
   return res.status(statusCode).json({
     message,
     status: success,
