cd4a3233fb0cb53c185b7f8c63375d72253ebe99
kim1037 <107454420+kim1037@users.noreply.github.com>
2025-05-28 14:59:30 +0800

    refactor: replace new Date with dayjs

--- src/utils/idGenerator.ts
+++ src/utils/idGenerator.ts
@@ -1,9 +1,14 @@
 type IdPrefix = "O" | "T";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export const generateId = (type: IdPrefix): string => {
-  const now = new Date();
-  const time = now.toISOString().slice(2, 19).replace(/[-T:]/g, "").slice(0, 12);
-  const randomNumber = Math.floor(Math.random() * 1000000)
+  const time = dayjs().tz("Asia/Taipei").format("YYMMDDHHmmss");
+  const randomNumber = Math.floor(Math.random() * 100000)
     .toString()
     .padStart(5, "0");
   return `${type}${time}${randomNumber}`;
