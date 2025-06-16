a73fd4d5d7a6dfc76c5e476ce2d8d9f98472ee35
洪于婷 <kimhong@hongyutingdeMacBook-Air.local>
2025-06-16 15:59:24 +0800

    feat: add upload category image api & modify category seed

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -1,12 +1,50 @@
 import type { NextFunction, Request, Response } from "express";
-import { getCategoriesData } from "../services/categoryService";
+import { categoryIdSchema } from "../schemas/zod/category.schema";
+import { getCategoriesData, getCategory, updateCategoryImg } from "../services/categoryService";
+import { uploadToCloudinary } from "../utils/cloudinary";
 import { sendResponse } from "../utils/sendResponse";
+import { validateInput } from "../utils/validateInput";
 
 // 取得所有類別
 export const getCategories = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const categories = await getCategoriesData();
     sendResponse(res, 200, "請求成功", true, categories);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const uploadCategoryImage = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const isAdmin = req.user?.email === process.env.ADMIN_EMAIL;
+    if (!isAdmin) {
+      sendResponse(res, 403, "帳號無管理員權限，無權編輯", false);
+      return;
+    }
+
+    const image = req.file;
+    const categoryId = validateInput(categoryIdSchema, req.params.categoryId);
+    const category = await getCategory(categoryId);
+    if (!category) {
+      sendResponse(res, 404, "類別不存在", false);
+      return;
+    }
+
+    if (!image) {
+      sendResponse(res, 400, "未上傳檔案", false);
+      return;
+    }
+
+    const imageUrl = await uploadToCloudinary(image.buffer, image.originalname, "categories");
+    await updateCategoryImg(Number(req.params.categoryId), imageUrl);
+
+    // 清除buffer
+    if (req.file) {
+      (req.file.buffer as unknown as Buffer | null) = null;
+    }
+
+    sendResponse(res, 200, "上傳成功", true, imageUrl);
   } catch (err) {
     next(err);
   }
--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -1,6 +1,18 @@
 import prisma from "../prisma/client";
-
+import type { CategoryId } from "../schemas/zod/category.schema";
 // 取得所有類別
 export const getCategoriesData = async () => {
   return await prisma.category.findMany();
+};
+
+// 取得單一類別
+export const getCategory = async (id: number) => {
+  return await prisma.category.findUnique({ where: { id } });
+};
+
+export const updateCategoryImg = async (categoryId: CategoryId, image: string) => {
+  return await prisma.category.update({
+    where: { id: categoryId },
+    data: { image },
+  });
 };
