6fab2e01a9b6431436815b79a94cf3fb54fe0678
Kim Hong <pan913063@gmail.com>
2025-06-24 00:58:53 +0800

    feat: add prisma log

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -7,7 +7,11 @@ const globalForPrisma = globalThis as {
 };
 
 // avoid multiple instances
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
