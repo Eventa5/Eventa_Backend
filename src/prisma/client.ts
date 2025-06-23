b2317b3bb1f790e53e559dce2c804257f3099429
Kim Hong <pan913063@gmail.com>
2025-06-24 03:32:59 +0800

    fix: close query log

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -10,7 +10,7 @@ const globalForPrisma = globalThis as {
 const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query", "info", "warn", "error"],
+    log: ["info", "warn", "error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
